0c4a21691850ccef29c84f2cf6cf04ebfd969f4c
thomas-dellacase <thomas.dellacase@laplateforme.io>
2022-03-29 10:52:12 +0200

    inscription connexion profils

---
+++ views/connexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <article>
+        <h1>Connexion</h1>
+    </article>
+    <article>
+        <form method="post" action="../views/profil.php"></form>
+            <input type="text" name="login" placeholder="Login"></input>
+            <input type="password" placeholder="Password"></input>
+        </form>
+    </article>
+</body>
+</html>
---
+++ views/inscription.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+    <article>
+        <h1>Inscription</h1>
+    </article>
+    <article>
+        <form method="post" action="../views/connexion.php"></form>
+            <input type="text" name="login" placeholder="Login"></input>
+            <input type="email" placeholder="Email"></input>
+            <input type="password" placeholder="Password"></input>
+            <input type="password2" placeholder="Confirmation de password"></input>
+        </form>
+    </article>
+</body>
+</html>
