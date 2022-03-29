ba8b6ce912a8b03f8012aa479f54496b945da9e0
Max <maxime.hadj@laplateforme.io>
2022-03-29 15:00:10 +0200

    commit 1

---
+++ views/conditions.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>    
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../assets/css/style.css"">
+        <title>Conditions générales de vente</title>
+    </head>
+
+        <body>
+
+            <h2>Conditions générales de vente</h2>
+                <p>Les informations des champs ci-dessous apparaîtront à plusieurs reprises dans les Conditions générales de vente.
+                Veuillez trouver ci-dessous nos conditions générales relatives à l'utilisation du site internet klapss.fr et à l'achat de produits sur ce site.
+                N'hésitez pas à nous faire part de toute question relative aux présentes conditions au moyen du formulaire de contact ci-dessous.
+                L'utilisation par les clients du site internet est régie par les présentes conditions générales ainsi que tout avis ou toutes conditions figurant sur une quelconque autre partie du site internet. Nous précisons à toutes fins utiles que Klapss peut modifier ou retirer à tout moment toute partie du site internet, sans que sa responsabilité ne puisse être engagée du fait de ces modifications à l'égard des clients. Klapss se réserve le droit de modifier les présentes conditions générales à l’avenir sans être tenue d’en aviser spécifiquement les clients. Le fait de poursuivre l’utilisation du site internet ou de passer commande ultérieurement à ces modifications vaudra acceptation de ces modifications.
+                Le contrat ne prendra effet entre vous (en qualité de client) et nous qu'une fois que la commande aura été traitée et expédiée.</p>
+
+                <p>En passant une commande au travers du présent site internet, le client garantit :</p>
+
+                <br>– qu'il n'est pas mineur ou dans l'incapacité légale de passer un contrat.</br>
+                <br>– que les coordonnées divulguées lors de l'enregistrement sont complètes et exactes.</br>
+                <br>– qu'il n'utilise pas un faux nom ou celui d'une autre personne physique ou morale sans autorisation.</br>
+
+            <h3>Passer une commande</h3>
+                <p>En passant une commande, vous acceptez les présentes conditions générales et reconnaissez les avoir lu avant de procéder à la commande.</p>
+                <p>Lorsqu'une commande est passée, vous recevrez une confirmation de commande à l'adresse e-mail fournie lors du paiement, contenant les informations sur le contenu et le prix de la commande.</p>
+
+            <h3>Annuler une commande</h3>
+                <p>En tant que client, vous pouvez annuler une commande sous 14 jours et vous serez remboursé du montant de cette dernière sur le compte avec lequel vous avez payé votre commande.</p>
+
+            <h3>Livraison</h3>
+                <p>En tant qu'acheteur, vous pouvez choisir parmi les options de livraison mentionnées et le moment qui vous convient le mieux. Klapss ne peut être tenu responsable de tout dommage causé pendant le transport ou dû à tout retard indépendant de sa volonté</p>
+
+            <h3>Politique de retour</h3>
+                <p>Klapss accepte les retours sous 14 jours si les articles n'ont pas été utilisés, modifiés, lavés ou autrement manipulés. Les articles doivent être retournés dans leur emballage d'origine. Les articles ne peuvent être retournés à Klapss sans le consentement écrit préalable de Klapss et sont soumis à des frais de retour.</p>
+
+            <h2>Politique de confidentialité</h2>
+                <p>La protection de votre vie privée est primordiale aux yeux de Klapss (ci-après dénommé indifféremment « Nom du Magasin du Client », « nous » et « nous », « contrôleur de données »). Nous veillons à la protection de votre vie privée lors de vos visites sur notre site internet ou pour les commandes passées en ligne.</p>
+
+                <p>Klapss fournit des produits et services et agit en qualité de responsable du traitement des données à caractère personnel que vous communiquez lorsque vous passez commande.
+                Vous pouvez contacter le Délégué à la protection des données de Klapss grâce au formulaire de contact qui se trouve au bas de cette page.</p>
+
+                <p>La présente politique en matière de protection de la vie privée définit la manière dont les données à caractère personnel vous concernant sont collectées, traitées et partagées lors de votre visite sur le site ou de votre commande sur Klapss.sumup.link (le « Site Internet »).</p>
+
+                <p>Quelles sont les données à caractère personnel que nous collectons ?</p>
+
+                <p>Lorsque vous réalisez ou essayez de réaliser un achat via le Site internet, nous collectons certaines données vous concernant, notamment votre nom, votre adresse de facturation, votre adresse de livraison, vos informations de paiement (y compris les numéros de carte de crédit), votre adresse e-mail et votre numéro de téléphone. Ces informations sont dénommées « Informations relatives à la commande ».</p>
+
+                <p>Lorsque vous visitez le Site internet, nous collectons automatiquement certaines informations sur votre appareil, y compris des données concernant votre navigateur internet, votre adresse IP, votre fuseau horaire ; certains cookies sont installés sur votre appareil. En outre, lorsque vous naviguez sur le Site internet, nous collectons des informations sur les pages ou les produits spécifiques que vous consultez, les sites ou termes de recherche qui vous ont redirigés vers le Site internet, ainsi que des données sur la manière dont vous interagissez avec le Site internet. Ces données collectées de manière automatisée sont dénommées « Informations relatives à l'appareil ».</p>
+
+                <p>Nous collectons des informations sur l'appareil en utilisant les technologies suivantes:</p>
+                <br>– Les «cookies» sont des fichiers de données qui sont placés sur votre appareil ou votre ordinateur ; ils contiennent généralement un identifiant unique anonyme.</br>
+                <br>– Les « fichiers journaux » suivent les activités sur le Site internet et collectent des données, y compris votre adresse IP, votre type de navigateur, votre fournisseur d'accès internet, les pages de renvoi / sortie et les horodatages.</br>
+                <br>– Les « beacons », « balises » ou « pixels » sont des fichiers électroniques utilisés pour enregistrer des informations sur la manière dont vous naviguez sur le Site internet.</br>
+
+            <h3>Comment nous traitons les données à caractère personnel vous concernant ?</h3>
+                <p>Les Informations relatives à la commande sont généralement traitées afin d'exécuter les commandes passées sur le Site internet (y compris afin de traiter vos informations de paiement, de gérer les modalités d'expédition et d'émettre les factures et/ou confirmation de commande). De plus, ces Informations relatives à la commande sont traitées afin de :</p>
+                <br>– communiquer avec vous</br>
+                <br>– contrôler les commandes afin de détecter les risques ou fraudes éventuelles et vous adresser des communications informatives ou publicitaires concernant nos produits et services si ces communications sont conformes aux préférences que vous avez partagées avec nous.</br>
+
+                <p>Les données vous concernant sont traitées afin de satisfaire à nos obligations contractuelles à votre égard (par exemple en cas de commande sur notre Site internet), ou pour défendre de toute autre façon nos intérêts commerciaux légitimes tels que listés ci-dessus.</p>
+                <p>Les informations relatives à l'appareil (en particulier votre adresse IP) sont collectées afin de détecter les risques et fraudes éventuelles, et plus généralement afin d'améliorer et optimiser le Site internet (en générant par exemple une analysée des données relatives à la manière dont nos utilisateurs naviguent et interagissent avec le Site internet), et pour évaluer l'impact de nos campagnes marketing et publicitaire.</p>
+
+            <h3>Partage des données à caractère personnel vous concernant</h3>
+                <p> Nous partageons les données à caractère personnel vous concernant avec des tiers afin de pouvoir les traiter conformément aux conditions définies ci-dessus. Par exemple, nous utilisons SumUp pour améliorer notre boutique en ligne. Nous utilisons également Google Analytics pour nous comprendre la manière dont nos clients utilisent le site – pour en savoir plus sur la façon dont Google utilise les données à caractère personnel vous concernant, consultez la page suivante : <a href="https://www.google.com/intl/en/policies/privacy/">Lien</a>. Vous pouvez désactiver Google Analytics en cliquant sur le lien suivant : <a href="https://tools.google.com/dlpage/gaoptout">Lien</a>.</p>
+                <p>Nous sommes susceptibles de partager des données dans le cadre de contrats conclus avec des prestataires de services qui contribuent à certaines de nos activités. Ces contrats stipulent que les prestataires en question sont en droit de traiter les données vous concernant uniquement aux fins des prestations qu'ils exécutent pour notre compte et s'interdisent de les utiliser pour leur bénéfice propre.</p>
+                <p>De même, nous sommes susceptibles de divulguer les données à caractère personnel vous concernant afin de répondre à une exigence légale ou réglementaire, à une citation à comparaître, a un mandat de perquisition et/ou à toute autre demande d'informations légitime que nous recevons, ou pour protéger nos droits de toute autre manière.</p>
+
+            <h3>Transfert international des données</h3>
+                <p>Nous sommes susceptibles de transférer les données collectées vous concernant à des tiers agissant pour notre compte situés dans des pays hors de l'Espace économique européen («EEE») ou dans des pays considérés par la Commission européenne comme offrant un niveau de protection des données satisfaisant. Il est possible que ces autres pays n'offrent pas le même niveau de protection aux données collectées vous concernant ; toutefois, nous continuerons à collecter, stocker et traiter les données vous concernant en tout temps conformément à la présente Politique en matière de protection de la vie privée et à la législation applicable en matière de protection des données. Nous veillerons à ce que les données ne soient partagées qu'avec des organisations offrant un niveau de protection des données approprié conformément à la législation applicable en matière de protection des données et à ce que des clauses contractuelles satisfaisantes soient conclues avec ces parties.</p>
+
+            <h3>Vos droits</h3>
+                <p>Si vous êtes soumis au RGPD, vous disposez d'un droit d'accès aux données à caractère personne que nous détenons vous concernant et d'un droit de rectification, de mise à jour et d'effacement de ces données ; vous disposez également du droit de demander l'arrêt du traitement des données vous concernant, de vous opposer profilage et au traitement automatisé des données vous concernant ; vous disposez en outre du droit à la limitation du traitement et au transfert de vos données (droit à la portabilité) dans certaines circonstances. Pour exercer l'un quelconque de ces droits, veuillez nous contacter grâce aux coordonnées figurant ci-dessous.</p>
+                <p>Vous avez le droit à tout moment de retirer votre consentement au traitement des données vous concernant dont la base légale est votre consentement, en contactant le Délégué à la protection des données (DPD) à l'adresse indiquée.</p>
+                <p>Pour exercer l'un quelconque de ces droits, veuillez nous contacter grâce au formulaire de contact figurant ci-dessous.</p>
+                <p>Vous disposez également du droit d'introduire une réclamation auprès de l'autorité de contrôle compétente.
+                    La liste de l'autorité compétente de chaque pays figure sur le site internet de la Communauté Européenne : <a href="http://ec.europa.eu/newsroom/article29/item-detail.cfm?item_id=612080">Lien</a></p>
+
+            <h3>Conservation des données</h3>
+                <p> Lorsque vous passez une commande au travers du Site internet, nous conserverons les Informations relatives à la commande aussi longtemps que nécessaire afin d'exécuter nos prestations ou aussi longtemps que les lois en vigueur l'exigent. Au terme de cette période, les données à caractère personnel vous concernant seront effacées.</p>
+
+            <h3>Modifications</h3>
+                <p>Nous sommes en droit de modifier ponctuellement la présente Politique en matière de protection de la vie privée, par exemple, afin de refléter des changements dans nos pratiques ou pour toute autre raison d'ordre opérationnel, légal ou réglementaire.</p>
+
+        </body>
+</html>
+
+
+
+
+
+
+
+
+
+
