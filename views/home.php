eadd13132ba23b512fa104245323dc789e4b28ea
Raphaël Diop <raphael.diop@laplateforme.io>
2022-03-29 15:37:41 +0200

    home/css

--- views/home.php
+++ views/home.php
@@ -5,15 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Klapss</title>
 </head>
 <body>
     <?php require "../requires/header.php" ?>
     <main>
-        <div class="imgAcceuil">
-            
+        <div class="imageAccueil">
+            <img src="https://cache.marieclaire.fr/data/photo/w1000_ci/5g/materiel-pinceau-peinture-bleu.jpg" alt="">
         </div>
+           <div class="textPre">
+                <h1>Bienvenue sur Klapss: La solution innovente à vos travaux de peinture.</h1>
+                <a href=""><button>Découvrir</button></a>
+            </div> 
+            
+            <div class="cards" >
+                <div class="card" style="width: 18rem;">
+                    <img src="" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img src="" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img src="" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+           
     </main>
-    
+
 </body>
 </html>
