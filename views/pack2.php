1f3ad3575900605eac76f64856774f7701a8cc9e
damien-verschaere <damien.verschaere@laplateforme.io>
2022-03-29 18:43:30 +0200

    branche damien mise en place

--- views/pack2.php
+++ views/pack2.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/style.css">
     <title>Klapss</title>
 </head>
 
 <body>
+    <header>
     <?php require "../requires/header.php" ?>
+    </header>
     <main class="P1">
         <div class="pack1 ms-5">
             <img src="../assets/img/champiface1.jpg" alt="Klapss champiface ">
