0111f2b815b2cbdb007421b69099e3af3793ccf1
damien-verschaere <damien.verschaere@laplateforme.io>
2022-03-30 13:55:42 +0200

    maj pack 3

---
+++ views/pack3.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Document</title>
+</head>
+
+<body class="bodyP1">
+    <header>
+        <?php require "../requires/header.php" ?>
+    </header>
+    <main class="P1">
+        <div class="pack1 ms-5">
+            <img src="../assets/img/pack3.png" alt="Klapss champiface ">
+            <div class="buttonP1 mx-3">
+                <h2 class="mx-5 mb-3 text-center">Applicateur de peinture avec pinceau + pinceau de rechange pré percé</h2>
+                <p>
+                    Prise en main facile pour les peintres professionnels ou non.
+                    Obtenez facilement une finition soignée, peignez sans déborder
+                    Réalisez rapidement des travaux d’angle, avec une grande précision.
+                    Une alternative aux adhésifs de protection. Fini les scotchs de masquage
+                    C’est l’accessoire idéal pour peindre les séparations entre 2 couleurs, permet des lignes précises et une bonne application de peinture.
+                </p>
+                <button>ajout au panier</button>
+                <button>achetez maintenant</button>
+            </div>
+        </div>
+        <h3>OBTENEZ FACILEMENT UNE FINITION SOIGNEE</h3>
+    </main>
+    <?php require "../requires/footer.php"; ?>
+</body>
+
+</html>
