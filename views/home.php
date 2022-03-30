318e0f7ac6e4dc716f6a31a3d48dc8fbcb7a877d
Raphaël Diop <raphael.diop@laplateforme.io>
2022-03-30 09:54:44 +0200

    home + css

--- views/home.php
+++ views/home.php
@@ -5,14 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Klapss</title>
 </head>
 <body>
-    <header>
-        <?php require "../requires/header.php" ?>
-    </header>
-        <main>
+    <?php require "../requires/header.php" ?>
+    <main>
         <div class="imageAccueil">
             <img src="https://cache.marieclaire.fr/data/photo/w1000_ci/5g/materiel-pinceau-peinture-bleu.jpg" alt="">
         </div>
@@ -21,32 +18,58 @@
                 <a href=""><button>Découvrir</button></a>
             </div> 
             
-            <div class="cards" >
-                <div class="card" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <section class="containerRef">
+            <h1>Nous sommes visibles sur:</h1>
+            <div class="refs">
+                <div class="video">
+                <iframe width="450" height="250" src="https://www.youtube.com/embed/6oP0-KP_5NU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
-
-                <div class="card" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="video">
+                <iframe width="450" height="250" src="https://www.youtube.com/embed/_q4_Pczk2QQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
-
-                <div class="card" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="video">
+                <iframe width="450" height="250" src="https://www.youtube.com/embed/6oP0-KP_5NU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
-           
+        </section>
+
+        <section class="containerProduit">
+            <div class="prodImage">
+                <img src="https://m.media-amazon.com/images/I/71HQqkWJF8L._AC_SX679_.jpg" alt="">
+            </div>
+            <div class="textProd1">
+                <h1>Champiface 3000</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima ut blanditiis voluptates dolores error, doloribus excepturi, totam nostrum aperiam accusantium vero dicta maxime quaerat aut illo veniam mollitia, sint at!</p>
+            </div class="text prod2">
+            <div>
+
+            </div>
+        </section>
+        
+        <div class="avisClient">
+            <h1><center>Un petit mot de nos clients</center></h1>
+            <div class="comClient">
+                <div class="avis">
+                    <img src="https://st2.depositphotos.com/3994049/7791/v/600/depositphotos_77912838-stock-illustration-quote-marks-icon-with-shadow.jpg" alt="Image guillemet">
+                    <p>"Un travail de qualité, rapide et soignée"</p>
+                    <h4>Stephan</h4>
+                </div>
+                <div class="avis">
+                <img src="https://st2.depositphotos.com/3994049/7791/v/600/depositphotos_77912838-stock-illustration-quote-marks-icon-with-shadow.jpg" alt="Image guillemet">
+                    <p>Contant très amicale, et très didactique</p>
+                    <h4>Paul</h4>
+                </div>
+                <div class="avis">
+                <img src="https://st2.depositphotos.com/3994049/7791/v/600/depositphotos_77912838-stock-illustration-quote-marks-icon-with-shadow.jpg" alt="Image guillemet">
+                    <p>Un travail efficace et une reponse diligente</p>
+                    <h4>Bernard</h4>
+                </div>
+            </div>
+        </div>
+            
     </main>
-    <div id='footer'>
-        <?php require "../requires/footer.php" ?>
-    </div>
+
+    <?php require "../requires/footer.php" ?>
+
 </body>
 </html>
